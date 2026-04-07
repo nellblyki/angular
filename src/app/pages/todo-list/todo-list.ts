bda2347891c174483306bc48686235c941f3a92f
nellblyki <artemii.kharlamov7@gmail.com>
2026-04-07 14:25:00 +0300

    complited Todo list

--- src/app/pages/todo-list/todo-list.ts
+++ src/app/pages/todo-list/todo-list.ts
@@ -8,9 +8,34 @@ import { FormsModule } from '@angular/forms';
 })
 export class TodoList {
   input = signal('');
-  todos = signal<string[]>(['fdsfdsfdsfsdfdsf']);
+  todos = signal<string[]>(['Покакать', 'Покушать']);
+  editValue = signal('');
+  editingIndex = signal<number | null>(null);
+  editingText = signal('');
   addTodo() {
     this.todos.update((prev) => [...prev, this.input()]);
-    this.input.set('')
+    this.input.set('');
+  }
+  deleteTodo(Todo: string) {
+    this.todos.update((todo) => todo.filter((todo) => todo !== Todo));
+  }
+  startEdit(index: number, value: string) {
+    this.editingIndex.set(index);
+    this.editingText.set(value);
+  }
+  saveTodo() {
+    const index = this.editingIndex();
+    if (index !== null) {
+      this.todos.update((prev) => {
+        const arr = [...prev];
+        arr[index] = this.editingText();
+        return arr;
+      });
+      this.cancelEdit();
+    }
+  }
+  cancelEdit() {
+    this.editingIndex.set(null);
+    this.editingText.set('');
   }
 }
